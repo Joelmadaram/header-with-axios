b08ee02f9d28e8c7a65a53a8649f7b199a56d35b
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-30 18:40:05 -0500

    added eventListener for the button

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 
 const jokes = document.querySelector('#jokes')
-const getDadJoke = async () => {
-    const config = {headers: {Accept: 'application/json'}}
-    const response = await axios.get('https://icanhazdadjoke.com/', config)
-    console.log(response.data.joke)
+const button = document.querySelector('button')
+
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke()
     const newLI = document.createElement('LI');
-    newLI.append(response.data.joke);
-    jokes.append(newLI);
+    newLI.append(jokeText);
+    jokes.append(newLI)
 }
 
-getDadJoke();
+
+const getDadJoke = async () => {
+    try {
+    const config = {headers: {Accept: 'application/json'}}
+    const response = await axios.get('https://icanhazdadjoke.com/', config)
+    return response.data.joke;
+    } catch (e) {
+        return "No Jokes available"
+    }
+}
+
+button.addEventListener('click',addNewJoke)
 
