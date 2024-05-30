e734e464da0c890922ca68ea347e238852cf2209
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-30 18:18:29 -0500

    added a click button and append a newLI

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+
+const jokes = document.querySelector('#jokes')
 const getDadJoke = async () => {
     const config = {headers: {Accept: 'application/json'}}
     const response = await axios.get('https://icanhazdadjoke.com/', config)
     console.log(response.data.joke)
+    const newLI = document.createElement('LI');
+    newLI.append(res.data.joke);
+    jokes.append(newLI);
 }
 
 getDadJoke();
